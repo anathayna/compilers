44fc9d30c7904caa0f037014a7c07c1ab1567e55
anathayna <anathaynafranca@gmail.com>
2021-03-01 21:17:56 -0300

    starting class b

--- class-b/src/main.rs
+++ class-b/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    print!("dois = {}", dois());
 }
+
+fn dois() -> i32 {
+    2
+}
+
+// cargo new class-b --bin/--lib
+// cargo run
+// rustup component/toolchain list | less
+// cargo build --release

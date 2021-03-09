6b819c7858b514a031c885a1bc7d614b38e528de
anathayna <anathaynafranca@gmail.com>
2021-03-08 21:31:29 -0300

    generic parser

--- class-b/src/main.rs
+++ class-b/src/main.rs
@@ -1,22 +1,31 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
+    println!("guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    println!("the secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    println!("please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
+        .expect("failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("you guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse::<u32>().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small!"),
+        Ordering::Greater => println!("too big!"),
+        Ordering::Equal => println!("you win!"),
+    }
 }
 
 

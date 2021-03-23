95f27fc5722c8525228aa37f8e26be0d38443504
ana thayna <anathaynafranca@gmail.com>
2021-03-22 23:59:50 -0300

    continue trace & log

--- class-c/lang42cli/src/main.rs
+++ class-c/lang42cli/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    //env_logger::init();
-    log::trace!("init version func");
+    env_logger::init();
+    log::trace!("init main func");
     //log::info!("aqui!");
     lang42lib::version();
-    log::trace!("end version func");
+    log::trace!("end main func");
 }
 
 //RUST_LOG=info target/debug/lang42cli
 //RUST_LOG=trace target/debug/lang42cli
+//RUST_LOG=trace cargo run
+//RUST_LOG=info,lang42lib=trace,lang42cli=trace cargo run
--- class-c/lang42lib/src/lib.rs
+++ class-c/lang42lib/src/lib.rs
@@ -1,5 +1,7 @@
 const VERSION: &str = "0.1.0";
 
 pub fn version() {
+    log::trace!("init version func");
     println!("version: {}", VERSION);
+    log::trace!("init version func");
 }

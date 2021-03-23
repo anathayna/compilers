1634963014e0a37216056c809dc3e847e91a3316
ana thayna <anathaynafranca@gmail.com>
2021-03-22 21:37:11 -0300

    trying...

--- class-d/sample-lexer-c/lexer.c
+++ class-d/sample-lexer-c/lexer.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-  return 0;
+    char *operation = "5 + 3*(2-5)/4 +3", *p = operation;
+
+    while (*p) { 
+        if ( isdigit(*p) || ((*p=='-'||*p=='+') && isdigit(*(p+1))) ) {
+            long val = strtol(p, &p, 10); 
+            printf("number(%ld)\n", val); 
+        } else {
+            p++;
+        }
+    }
+
+    char buf[] ="5 + 3*(2-5)/4 +3";
+    int i = 0;
+    char *space = strtok (buf, " ");
+    char *array[3];
+
+    while (space != NULL) {
+        array[i++] = space;
+        space = strtok (NULL, "/");
+    }
+
+    for (i = 0; i < 3; ++i) 
+        printf("(%s)\n", array[i]);
+
+    return 0;
 }
+
+// gcc lexer.c
+// ./a.out

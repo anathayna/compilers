8a5bd104a51eea2e7b88995d2174e573bf8ceda8
ana thayna <anathaynafranca@gmail.com>
2021-04-05 21:32:13 -0300

    item lexico

--- class-d/sample-lexer-rust/src/main.rs
+++ class-d/sample-lexer-rust/src/main.rs
@@ -8,4 +8,22 @@ fn main() {
         eprintln!("sintaxe: ehbasic <expressão>");
         process::exit(1);
     }
+
+    let simbolos = lexer(&argv[1]);
+    println!("{:?}", simbolos)
 }
+
+#[derive(Debug)]
+enum ItemLexico {
+    Numero(u64)
+}
+
+fn lexer(exp: &String) -> Vec<ItemLexico> {
+    //unimplemented!();
+    let mut r = Vec::new();
+    let numero = exp.parse().unwrap();
+    r.push(ItemLexico::Numero(numero));
+    r
+}
+
+// cargo run -- 9
